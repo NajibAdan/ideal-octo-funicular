d5a429443e6eec617517a90a668f88b42303f904
NajibAdan <najibadan1@gmail.com>
2017-07-07 19:40:17 +0300

    Added a new show to the list

--- lewd.py
+++ lewd.py
@@ -11,11 +11,11 @@ def download_and_open(i):
 url = "https://nyaa.si/rss?c=1_2&f=0"
 search_url = "https://nyaa.si/rss?c=1_2&f=0&q=%title%"
 feed = feedparser.parse(url).entries
-airing_shows = ["Fate","Shingeki no Bahamut","Kakegurui","Musekinin"]
+airing_shows = ["Fate","Shingeki no Bahamut","Kakegurui","Musekinin","Ballroom e Youkoso"]
 for  i in feed:
     for x in airing_shows:
         if x in i.title and "720" in i.title:
             pprint(i.title)
             download_and_open(i)
 while 1:
-    time.sleep(1800)
+    time.sleep(180)
