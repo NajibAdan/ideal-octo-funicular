1c8d779f888e0f01abe0123ebb07d3e71a2062dc
NajibAdan <najibadan1@gmail.com>
2017-07-07 19:45:06 +0300

    Added a checking system to avoid downloading the same file if it has already been downloaded

--- lewd.py
+++ lewd.py
@@ -5,8 +5,12 @@ import os
 import time
 
 def download_and_open(i):
-    wget.download(i.link, "C:\Users\Dank\Documents\Animu Torrents\\"+i.title+".torrent")
-    os.startfile("C:\Users\Dank\Documents\Animu Torrents\\"+i.title+".torrent")
+    if os.path.isfile("C:\Users\Dank\Documents\Animu Torrents\\"+i.title+".torrent"):
+        print i.title + ' has already been downloaded'
+    else:
+        print 'Downloading '+ i.title
+        wget.download(i.link, "C:\Users\Dank\Documents\Animu Torrents\\"+i.title+".torrent")
+        os.startfile("C:\Users\Dank\Documents\Animu Torrents\\"+i.title+".torrent")
 
 url = "https://nyaa.si/rss?c=1_2&f=0"
 search_url = "https://nyaa.si/rss?c=1_2&f=0&q=%title%"
@@ -15,7 +19,7 @@ airing_shows = ["Fate","Shingeki no Bahamut","Kakegurui","Musekinin","Ballroom e
 for  i in feed:
     for x in airing_shows:
         if x in i.title and "720" in i.title:
-            pprint(i.title)
+
             download_and_open(i)
 while 1:
     time.sleep(180)
