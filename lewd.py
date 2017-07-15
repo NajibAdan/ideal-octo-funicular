2245d3f2b7e1a7b73a27f54571084b01135f261b
NajibAdan <najibadan1@gmail.com>
2017-07-15 15:35:52 +0300

    Changed list
    It creates a dictionary of shows I'm watching and the next episode
    instead of me writing a list of shows I'll be watching

--- lewd.py
+++ lewd.py
@@ -3,7 +3,7 @@ from pprint import  pprint
 import wget
 import os
 import time
-
+import mal
 def download_and_open(i):
     if os.path.isfile("\Animu Torrents\\"+i.title+".torrent"):
         print i.title + ' has already been downloaded'
@@ -11,15 +11,14 @@ def download_and_open(i):
         print 'Downloading '+ i.title
         wget.download(i.link, "Animu Torrents\\"+i.title+".torrent")
         os.startfile(".\Animu Torrents\\"+i.title+".torrent")
-
-
+airing_shows = mal.placeholder()
 while 1:
+
     time.sleep(10)
     url = "https://nyaa.si/rss?c=1_2&f=0"
     search_url = "https://nyaa.si/rss?c=1_2&f=0&q=%title%"
     feed = feedparser.parse(url).entries
-    airing_shows = ["Fate","Shingeki no Bahamut","Kakegurui","Musekinin","Ballroom e Youkoso"]
     for  i in feed:
-        for x in airing_shows:
-            if x in i.title and "720" in i.title:
+        for x in airing_shows.keys():
+            if show in i.title and "720" in i.title:
                 download_and_open(i)
