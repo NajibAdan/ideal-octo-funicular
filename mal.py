bb9fb9e7e88b4eafa3d66970ce831a166da71f8d
NajibAdan <najibadan1@gmail.com>
2017-07-15 15:33:37 +0300

    Changed API
    Moved from using MAL's official API to Atarashi's API

--- mal.py
+++ mal.py
@@ -1,9 +1,15 @@
-import xmltodict
+import json
 import requests
-from pprint import pprint
+def placeholder():
+    dict = {}
+    username = raw_input('Enter MAL username: ')
+    password = raw_input('Enter password: ')
 
-url = "https://myanimelist.net/api/anime/search.xml?q=bleach"
-r = requests.get(url, auth =(username,password))
-result= xmltodict.parse(r.content)
-for i in result['anime']['entry']:
-    print(i['title'])
+    url = 'http://localhost:8000/2.1/animelist/' + username
+    idurl = 'http://localhost:8000/2.1/anime/'
+    r = requests.get(url,auth=(username,password))
+    response = json.loads(r.text)
+    for i in response['anime']:
+        if i['status'] == 'currently airing' and i['watched_status'] == 'watching':
+            dict[i['title']] = i['watched_episodes']+1
+    return dict
