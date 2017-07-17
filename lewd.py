115d0d4361a9e07fbf3db11c43780e23e863873a
NajibAdan <najibadan1@gmail.com>
2017-07-17 12:31:29 +0300

    Searches for the show using a url instead of checking the new releases RSS

--- lewd.py
+++ lewd.py
@@ -13,13 +13,16 @@ def download_and_open(i):
         os.startfile(".\Animu Torrents\\"+i.title+".torrent")
 airing_shows = mal.placeholder()
 while 1:
-
-    time.sleep(10)
     url = "https://nyaa.si/rss?c=1_2&f=0"
-    search_url = "https://nyaa.si/rss?c=1_2&f=0&q=%title%"
-    feed = feedparser.parse(url).entries
-    for  i in feed:
-        for x in airing_shows.keys():
-            show = x + ' ' + str(airing_shows[x])
-            if show in i.title and "720" in i.title:
-                download_and_open(i)
+    for x in airing_shows.keys():
+        search_url = "https://nyaa.si/rss?c=1_2&f=0&q="
+        search_url = search_url + x
+        feed = feedparser.parse(search_url).entries
+        for i in feed:
+            show = x+' '+str(airing_shows[x])
+            if show in i.title and '720p' in i.title:
+                print(repr(i.title)) + 'torrent found'
+                download_and_open(i.title)
+            else:
+                print 'No new torrents found'
+                time.sleep(10)
